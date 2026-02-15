e836e5484d3f40a4e0ac26fc476b70e86e2dfc91
Kripa Hayanju <uwi001@Kripas-MacBook-Air.local>
2026-02-14 21:49:19 -0600

    1

---
+++ qn1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+/*********************************************************************
+File name: qn1.cpp 
+Author: KRIPA HAYANJU
+Date: 09/18/2025
+
+Purpose:
+    The program reads movie data from a file and stores it in an array of structures. 
+    It allows tracking the number of times each movie has been watched, computes the total runtime of all movies, and identifies the least-watched movie.
+Command Parameters:
+    No parameters to main().
+Input:
+    The user provides the file name, selects movies to watch by entering their serial number, and exit the program by typing “quit".
+Results:
+    The program displays the list of movies, along with the total runtime of all movies and the title of the least-watched movie.
+Notes:
+    The program uses stoi function to turn string into integer. The program will allow invalid input and coninue running till the user inputs "quit".
+*********************************************************************/
+
+
+class ArrayList
+{
+    private:
+        int MAX_SIZE = 100;
+    
+    public: 
+        void insert(int iValue)
+        {
+
+        }
+
+        void printList()
+        {
+
+        }
+
+        int access(int iIndex) 
+        {
+
+        }
+};
+
+int main()
+{
+    int iNum;
+    cout << "Enter number of elements: ";
+    cin >> iNum;
+
+}
